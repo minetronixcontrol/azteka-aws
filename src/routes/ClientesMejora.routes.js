83754d2f40c6458045579c0e9d2a63b55ff54d00
minetronixcontrol <133145949+minetronixcontrol@users.noreply.github.com>
2024-06-10 16:11:15 -0600

    ultimas modficaciones a producion

--- src/routes/ClientesMejora.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'); //requerimiento de express
-const router = express.Router();   // invocacion de la funcion router del modulo express
-const Cliente = require('../models/Clientes');//Invocacion de modelo de datos de mongo de cliente
-
-router.get('/:nombre:/apaterno', asyc(req,res)=>{
-
-    
-})
